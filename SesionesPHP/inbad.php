9c18583b51d4c3eadaaaf4b5707c4885576ab5d3
jmartinpacheco <martinpacheco1192@gmail.com>
2014-10-14 05:39:53 -0500

    enlace inbad.php

---
+++ SesionesPHP/inbad.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Mal acceso</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+</head>
+<body>
+	<div class="col-md-4">
+		<h1>Escribe en algun campo</h1>
+	<a href="index.php" class="btn btn-primary">volver</a>	
+	</div>	
+</body>
+</html>
