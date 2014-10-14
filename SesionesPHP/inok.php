dd468263868ed8bc27fc5af3e5fbf7101a87212e
jmartinpacheco <martinpacheco1192@gmail.com>
2014-10-14 05:44:06 -0500

    enlace inok.php

---
+++ SesionesPHP/inok.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Acceso</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+</head>
+<body>
+	<div class="col-md-4">
+		<h1>Bienvenido</h1>
+		<br>	
+		<h4>Lee este texto</h4>
+		<br>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores harum sapiente at enim minus ex magnam molestias iste atque vitae voluptatum, sed et quas itaque in, adipisci sunt. Odit, nobis.</p>
+	<a href="index.php" class="btn btn-primary">Cerrar Sesion</a>	
+	</div>	
+</body>
+</html>
